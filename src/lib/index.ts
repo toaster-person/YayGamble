d3187f388f0b227f5c64390f9e82eb11d2cb7ca5
YayBlaze <blazeannison@icloud.com>
2025-12-03 14:22:44 -0800

    fix: fixed ip logging to not add duplicates

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,8 +2,9 @@ import { dbQuery } from './db';
 
 export async function updateIPs(id: string, ip: string): Promise<void> {
 	try {
-		const res = await dbQuery('SELECT ip FROM ips WHERE id = ?', [id]);
-		if (ip == '137.83.106.214' || ip in res) return;
+		const res: any[] = await dbQuery('SELECT ip FROM ips WHERE id = ?', [id]);
+		const ips = res.map((item) => item.ip);
+		if (ip == '137.83.106.214' || ips.includes(ip)) return;
 		const usr = (await dbQuery('SELECT username FROM users WHERE id = ?', [id]))[0].username;
 		await dbQuery('INSERT INTO ips (id, username, ip) VALUES (?, ?, ?)', [id, usr, ip]);
 	} catch (err: any) {
