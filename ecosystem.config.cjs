86f4f16ade48a5666e0613c414e7bbd1214ee791
YayBlaze <blazeannison@icloud.com>
2025-12-05 11:53:53 -0800

    feat: added quick stop for slots

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,9 +3,8 @@ module.exports = {
 		{
 			name: 'gamble',
 			script: './build/index.js',
-			interpreter: 'bun',
+			interpreter: '/home/blaze/.bun/bin/bun', // insert your own interpreter here
 			env: {
-				PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
 				PORT: '5301',
 				ORIGIN: 'http://127.0.0.1'
 			}
